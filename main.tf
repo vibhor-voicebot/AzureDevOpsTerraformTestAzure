4671b1f50fe0767c1f708b7596fbf23efacb2b7e
vibhor-voicebot <53684148+vibhor-voicebot@users.noreply.github.com>
2023-11-20 19:02:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
     container_name       = "tstate"
     key                  = "77Q4LUB5o9wRdbPYDt+0kGZP+L8Sj9E/FNXg7lZBQS5z3mLod5cyan4wA19CR1SmlqIRUFQfhuQrPVaGzNhjGw=="
   }
-
+ 
   required_providers {
     azurerm = {
       # Specify what version of the provider we are going to utilise
