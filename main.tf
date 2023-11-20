f7448305ae45afcb1ba3cf80cc9be12c8f4ed530
vibhor-voicebot <53684148+vibhor-voicebot@users.noreply.github.com>
2023-11-20 18:31:06 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "iacqaautomation-infra"
     storage_account_name = "iacqaautomationtstate"
     container_name       = "tstate"
-    key                  = "77Q4LUB5o9wRdbPYDt+0kGZP+L8Sj9E/FNXg7lZBQS5z3mLod5cyan4wA19CR1SmlqIRUFQfhuQrPVaGzNhjGw=="
+    key                  = "0seHMQKA6FPH3uH4HhHXeF8x7XyIYTU8uygyV9uS8KSPIA7UFVKPoGPktNGV0KfYvLHS7tgcnrUt+AStngpq1w=="
   }
  
   required_providers {
